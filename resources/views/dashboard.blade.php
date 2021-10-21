8201e219cb71c78fa9354fd3a23f923010c5be37
maruf-229 <79322030+maruf-229@users.noreply.github.com>
2021-10-21 18:53:04 +0600

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            HI...{{ Auth::user()->name }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+
+    </div>
+</x-app-layout>
