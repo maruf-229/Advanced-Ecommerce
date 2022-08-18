2d3794b0d1d5ba71f6a3bfab9129c5d5573b2e10
maruf-229 <79322030+maruf-229@users.noreply.github.com>
2022-08-19 00:26:53 +0600

    uppp-2

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -101,20 +101,23 @@
                     </ul>
                 </li>
 
-                @php
-                    $adminData = DB::table('admins')->first();
-                    @endphp
+            @php
+                use Illuminate\Support\Facades\DB;
+                $adminData = DB::table('admins')->first();
+            @endphp
 
-                <!-- User Account-->
+
+            <!-- User Account-->
                 <li class="dropdown user user-menu">
                     <a href="#" class="waves-effect waves-light rounded dropdown-toggle p-0" data-toggle="dropdown" title="User">
-                        <img src="{{ (!empty($adminData->profile_photo_path))? url('upload/admin_images/'.$adminData->profile_photo_path):
-                                url('upload/no_image.jpg') }}" alt="">
+                        <img src="{{ (!empty($adminData->profile_photo_path))? url('upload/admin_images/'.$adminData->profile_photo_path):url('upload/no_image.jpg') }}" alt="">
                     </a>
                     <ul class="dropdown-menu animated flipInX">
                         <li class="user-body">
                             <a class="dropdown-item" href="{{ route('admin.profile') }}"><i class="ti-user text-muted mr-2"></i> Profile</a>
-                            <a class="dropdown-item" href="{{ route('admin.change.password') }}"><i class="ti-wallet text-muted mr-2"></i>Change Password</a>
+
+                            <a class="dropdown-item" href="{{ route('admin.change.password') }}"><i class="ti-wallet text-muted mr-2"></i> Change Password </a>
+
                             <a class="dropdown-item" href="#"><i class="ti-settings text-muted mr-2"></i> Settings</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="{{ route('admin.logout') }}"><i class="ti-lock text-muted mr-2"></i> Logout</a>
